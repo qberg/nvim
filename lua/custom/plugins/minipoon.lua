96757dfe3277cefd5a553f337ba21a09394da7ec
qberg <qbergnoe@gmail.com>
2026-03-23 09:02:05 +0530

    feat: made minipoon toggle better and changed fundamental state management

--- lua/custom/plugins/minipoon.lua
+++ lua/custom/plugins/minipoon.lua
@@ -1,23 +1,15 @@
 return {
   {
     dir = vim.fn.stdpath 'config' .. '/plugins/minipoon',
-
     name = 'minipoon',
-
     keys = {
-      {
-        '<leader>ma',
-        function()
-          require('minipoon').add_file()
-        end,
-        desc = 'MARK BUFFER',
-      },
+      -- Menu and System Ops
       {
         '<leader>mh',
         function()
           require('minipoon').toggle_menu()
         end,
-        desc = 'VIEW LIST',
+        desc = 'MINIPOON UI',
       },
       {
         '<leader>mx',
@@ -27,6 +19,51 @@ return {
         desc = 'PURGE LIST',
       },
 
+      -- Set Marks (The Precision Overrides)
+      {
+        '<leader>m1',
+        function()
+          require('minipoon').set_mark(1)
+        end,
+        desc = 'MARK SLOT [1]',
+      },
+      {
+        '<leader>m2',
+        function()
+          require('minipoon').set_mark(2)
+        end,
+        desc = 'MARK SLOT [2]',
+      },
+      {
+        '<leader>m3',
+        function()
+          require('minipoon').set_mark(3)
+        end,
+        desc = 'MARK SLOT [3]',
+      },
+      {
+        '<leader>m4',
+        function()
+          require('minipoon').set_mark(4)
+        end,
+        desc = 'MARK SLOT [4]',
+      },
+      {
+        '<leader>m5',
+        function()
+          require('minipoon').set_mark(5)
+        end,
+        desc = 'MARK SLOT [5]',
+      },
+      {
+        '<leader>m6',
+        function()
+          require('minipoon').set_mark(6)
+        end,
+        desc = 'MARK SLOT [6]',
+      },
+
+      -- Jump to Marks
       {
         '<leader>1',
         function()
@@ -55,10 +92,24 @@ return {
         end,
         desc = 'JUMP [4]',
       },
+      {
+        '<leader>5',
+        function()
+          require('minipoon').nav_file(5)
+        end,
+        desc = 'JUMP [5]',
+      },
+      {
+        '<leader>6',
+        function()
+          require('minipoon').nav_file(6)
+        end,
+        desc = 'JUMP [6]',
+      },
     },
 
     config = function()
-      print '>> MINIPOON SYSTEM ONLINE <<'
+      print '>> MINIPOON V2 ONLINE <<'
     end,
   },
 }
--- plugins/minipoon/lua/minipoon.lua
+++ plugins/minipoon/lua/minipoon.lua
@@ -1,48 +1,58 @@
 local M = {}
-M.marks = {}
 
-function M.add_file()
+-- Initialize exactly 6 empty slots
+M.marks = { nil, nil, nil, nil, nil, nil }
+
+function M.set_mark(index)
   local file = vim.fn.expand '%:p'
   if file == '' then
+    print '[!] CANNOT MARK EMPTY BUFFER'
     return
   end
 
-  for _, f in ipairs(M.marks) do
-    if f == file then
-      print '(!) ALREADY MARKED (hence the yelling)'
-      return
-    end
-  end
-
-  table.insert(M.marks, file)
-  print('[+] BUFFER MARKED :: ' .. vim.fn.fnamemodify(file, ':t'))
+  M.marks[index] = file
+  print(string.format('>> SLOT [%d] OVERWRITTEN :: %s <<', index, vim.fn.fnamemodify(file, ':t')))
 end
 
 function M.toggle_menu()
-  if #M.marks == 0 then
-    print '[!] LIST EMPTY'
-    return
+  local items = {}
+
+  -- Format the 6 slots for the UI picker
+  for i = 1, 6 do
+    local file = M.marks[i]
+    local display_name = file and vim.fn.fnamemodify(file, ':t') or 'EMPTY'
+    table.insert(items, string.format('[%d] %s', i, display_name))
   end
 
-  print '>> JUMP LIST <<'
-  for i, file in ipairs(M.marks) do
-    -- Format: [1] filename.lua
-    print('[' .. i .. '] ' .. vim.fn.fnamemodify(file, ':t'))
-  end
+  -- vim.ui.select will be intercepted by Telescope automatically
+  vim.ui.select(items, {
+    prompt = '>> MINIPOON JUMP <<',
+    format_item = function(item)
+      return item
+    end,
+  }, function(choice, idx)
+    -- This callback runs when you press Enter in Telescope
+    if not choice then
+      return -- User aborted/pressed Esc
+    end
+
+    -- The index of the selected item perfectly matches our 1-6 slots
+    M.nav_file(idx)
+  end)
 end
 
 function M.nav_file(index)
   local file = M.marks[index]
-  if file then
+  if file and file ~= '' then
     vim.cmd('e ' .. file)
   else
-    print '[!] INVALID INDEX'
+    print(string.format('[!] SLOT [%d] IS EMPTY', index))
   end
 end
 
 function M.clear_all()
-  M.marks = {}
-  print '[*] SYSTEM PURGED'
+  M.marks = { nil, nil, nil, nil, nil, nil }
+  print '[*] SYSTEM PURGED: ALL SLOTS CLEARED'
 end
 
 return M
